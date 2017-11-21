c06be72985b40a2e109825bb2cf4c8f4f5048a4b
Herman <zeesun.wu@gmail.com>
2017-11-20 20:41:19 -0800

    add tinyUrl

---
+++ src/EncodeAndDecodeTinyUrl.java
@@ -0,0 +1,18 @@
+/**
+ * Created by hermanwu on 11/14/17.
+ */
+public class EncodeAndDecodeTinyUrl {
+    public static void main(String[] args) {
+        EncodeAndDecodeTinyUrl s = new EncodeAndDecodeTinyUrl();
+    }
+
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+
+    }
+}
