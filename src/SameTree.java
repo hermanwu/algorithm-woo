6023157a9472b7da08daea6589a96765fd0cef33
Herman <zeesun.wu@gmail.com>
2017-11-30 16:13:46 -0800

    add problem SameTree

---
+++ src/SameTree.java
@@ -0,0 +1,17 @@
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        }
+
+        return isSameTree(p.left, p.left) && isSameTree(q.right, q.right);
+    }
+}
---
+++ src/TreeGenerator.java
@@ -0,0 +1,5 @@
+/**
+ * Created by hermanwu on 11/30/17.
+ */
+public class TreeGenerator {
+}
