da7929ce75b7c996bc801f8ef4ac3d326e1eb944
Herman <zeesun.wu@gmail.com>
2017-12-04 17:43:44 -0800

    coins in a line

--- src/CoinsInALine.java
+++ src/CoinsInALine.java
@@ -1,18 +1,57 @@
 // Input:
-// Two perosn pick up rock from two sides
+// You have n coins in a line
+//
+// Two player starts to pick up coins from two sides.
+// Each players can pick one either one coin or two coins
 //
 
 // Output:
-// the fewest number of coins that can add together to the amount;
+// whether the first player will win
 
+// Example:
+// n = 4;
+// answer: true;
 
+// n = 5;
+// answer: false;
 
 public class CoinsInALine {
-    public static void main(String[] args) {
-        CoinsInALine cc = new CoinsInALine();
+    public boolean firstWillWin(int n) {
+        // for current player, if there is not coins, he lost.
+        if (n == 0) {
+            return false;
+        }
+
+        if (n < 2) {
+            return true;
+        }
+
+        boolean[] f = new boolean[n + 1];
+
+        // When there is 1 coin, current player wins;
+        f[1] = true;
+        // When there are 2 coins, current play wins;
+        f[2] = true;
+        // When there are 3 coins, no matter whether he picked one or two coins, the other play will pick the rest;
+        f[3] = false;
+
+        for (int i = 4; i <= n; i++) {
+            // whether player picks 1 coin or 2 coins ,
+            // if the other player loses ((f[i - 1]) || (f[i - 2]) == false),
+            // then current player wins
+            if (f[i - 1] == false || f[i - 2] == false) {
+                f[i] = true;
+            }
+        }
+
+        return f[n];
     }
 
-    public boolean firstWillWin(int n) {
-        return true;
+    public static void main(String[] args) {
+
+        CoinsInALine cc = new CoinsInALine();
+
+        System.out.println(cc.firstWillWin(4));
     }
+
 }
