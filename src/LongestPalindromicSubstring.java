aed571069b8aba680ea8ae7468328c524685e5b5
Herman Wu <zeesun.wu@gmail.com>
2017-12-31 18:47:30 -0800

    longest palindromic substring

--- src/LongestPalindromicSubstring.java
+++ src/LongestPalindromicSubstring.java
@@ -16,6 +16,71 @@
 
  */
 public class LongestPalindromicSubstring {
-    public int longestPalindromeSubseq(String s) {
+    public static void main(String[] args) {
+        LongestPalindromicSubstring l = new LongestPalindromicSubstring();
+
+        Printer.printResultComparsion(l.longestPalindrome("babad"), "bab");
     }
-}
+
+    public String longestPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length, i, maxLen = 0;
+        String res = "";
+
+        for (i = 0; i < n; i++) {
+            // check string two characters bigger than current max;
+            if (isPalindrome(s, i - maxLen - 1, i)) {
+                res = s.substring(i - maxLen - 1, i + 1);
+                maxLen = maxLen + 2;
+            }
+
+            // check string one character bigger than current max;
+            if (isPalindrome(s, i - maxLen, i)) {
+                res = s.substring(i - maxLen, i + 1);
+                maxLen = maxLen + 1;
+            }
+        }
+
+        return res;
+    }
+
+    public boolean isPalindrome(String s, int start, int end) {
+        if (start < 0 || end >= s.length()) {
+            return false;
+        }
+
+        while (start < end) {
+            if (s.charAt(start) == s.charAt(end)) {
+                start++;
+                end--;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String longestPalindromeUsingDp(String s) {
+        int n = s.length();
+
+        String result = null;
+
+        boolean[][] dp = new boolean[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if ((i - j) <= 2 || dp[i - 1][j + 1]) {
+                        dp[i][j] = true;
+
+                        if (result == null ||
+                                (i - j + 1 > result.length())) {
+                            result = s.substring(j, i+1);
+                        }
+                    }
+                }
+            }
+        }
+
+        return result;
+    }}
