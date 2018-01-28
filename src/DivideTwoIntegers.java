6a2489c2835212ad4b9395148352aebbc9fac5d0
Herman Wu <zeesun.wu@gmail.com>
2018-01-27 17:34:54 -0800

    divide two integers

--- src/DivideTwoIntegers.java
+++ src/DivideTwoIntegers.java
@@ -1,5 +1,51 @@
 /**
- * Created by hermanwu on 1/9/18.
+ Divide two integers without using multiplication, division and mod operator.
+
+ If it is overflow, return MAX_INT.
  */
+
 public class DivideTwoIntegers {
+    public int divide(int dividend, int divisor) {
+        int sign = 1;
+
+        if ((dividend > 0 && divisor < 0)||(dividend < 0 && divisor > 0)) {
+            sign = -1;
+        }
+
+        long ldividend = Math.abs((long) dividend);
+        long ldivisor = Math.abs((long) divisor);
+
+        if (ldivisor == 0) {
+            return Integer.MAX_VALUE;
+        }
+
+        if ((ldividend == 0) || (ldividend < ldivisor)) {
+            return 0;
+        }
+
+        long lans = longDivide(ldividend, ldivisor);
+
+        int ans;
+        if (lans > Integer.MAX_VALUE) {
+            ans = (sign == 1)? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        } else {
+            ans = (int) (sign * lans);
+        }
+
+        return ans;
+    }
+
+    private long longDivide(long dividend, long divisor) {
+        if (dividend < divisor) return 0;
+
+        long sum = divisor;
+        long multiple = 1;
+
+        while ((sum + sum) <= dividend){
+            sum += sum;
+            multiple += multiple;
+        }
+
+        return multiple + longDivide(dividend - sum, divisor);
+    }
 }
