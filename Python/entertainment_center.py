50936f08f479cd3a3861b6b554a57ec24709b262
Herman Wu <zeesun.wu@gmail.com>
2018-02-13 21:44:48 -0800

    word search

---
+++ Python/entertainment_center.py
@@ -0,0 +1,8 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+                        "A story of a boy and his toys that come to life",
+                        "www",
+                        "trailer url")
+
+print(toy_story.storyline)
