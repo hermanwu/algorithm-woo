09ff38cff7a4ba84c02be2f7eab7a44836084261
Herman <zeesun.wu@gmail.com>
2018-02-22 21:18:08 -0800

    reverse linked list

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,7 +1,21 @@
-package PACKAGE_NAME;
-
 /**
  * Created by hermanwu on 2/22/18.
  */
 public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+
+        ListNode newHead = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = temp;
+        }
+
+        return newHead;
+    }
 }
