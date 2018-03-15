25d8bb6c08a884830990d79eac36d426d0154730
Herman <zeesun.wu@gmail.com>
2018-03-15 10:00:17 -0700

    shape factory

---
+++ python/shape_factory.py
@@ -0,0 +1,36 @@
+class Shape:
+    def draw(self):
+        raise NotImplementedError('This method should have implemented.')
+
+class Triangle(Shape):
+    # Write your code here
+    def draw(self):
+        print "  /\\"
+        print " /  \\"
+        print "/____\\"
+
+class Rectangle(Shape):
+    # Write your code here
+    def draw(self):
+        print " ----"
+        print "|    |"
+        print " ----"
+
+class Square(Shape):
+    # Write your code here
+    def draw(self):
+        print " ----"
+        print "|    |"
+        print "|    |"
+        print " ----"
+
+class ShapeFactory:
+    def getShape(self, shapeType):
+        if shapeType == "Triangle":
+            return Triangle()
+        elif shapeType == "Rectangle":
+            return Rectangle()
+        elif shapeType == "Square":
+            return Square()
+        else:
+            return None
