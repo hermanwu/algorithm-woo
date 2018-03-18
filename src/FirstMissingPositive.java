8cf6ec37461625b5072e50a69e91183f17e4976b
Herman <zeesun.wu@gmail.com>
2018-03-17 23:23:38 -0700

    first bad version java

--- src/FirstMissingPositive.java
+++ src/FirstMissingPositive.java
@@ -14,7 +14,8 @@ public class FirstMissingPositive {
         if (nums == null || nums.length == 0) return 1;
 
         for (int i = 0; i < nums.length; i++) {
-            while (nums[i] > 0 && nums[i] <= nums.length && nums[nums[i] - 1] != nums[i]) {
+            while (nums[i] > 0 && nums[i] <= nums.length &&
+                    nums[nums[i] - 1] != nums[i]) {
                 int temp = nums[nums[i] - 1];
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
