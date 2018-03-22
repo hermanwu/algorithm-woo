2d1877d0aea7041efd0a707197ea7a11014b0a0d
Herman <zeesun.wu@gmail.com>
2018-03-21 17:27:14 -0700

    is valid BST non recursive travsal

--- src/IsValidBST.java
+++ src/IsValidBST.java
@@ -2,9 +2,36 @@
  * Created by hermanwu on 3/21/18.
  */
 public class IsValidBST {
-    public boolean isValidBST(TreeNode root) {
+    public boolean isValidBST2(TreeNode root) {
         return helper(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
+
+    public boolean isValidBST(TreeNode root) {
+        // use pre to compare
+        TreeNode pre = null;
+
+        Stack<TreeNode> stack = new Stack<>();
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+
+            // compare with pre and set pre
+            if (pre != null && pre.val >= root.val) {
+                return false;
+            }
+            pre = root;
+
+            root = root.right;
+        }
+
+        return true;
+    }
+
 
     public boolean helper(TreeNode node, long min, long max) {
         if (node == null) {
