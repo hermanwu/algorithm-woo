5fff0f53fa39313da49c6580f2fc947506c03196
Herman Wu <zeesun.wu@gmail.com>
2018-03-24 04:35:48 -0700

    target sum - java - dp

--- src/TargetSum.java
+++ src/TargetSum.java
@@ -31,4 +31,34 @@ public class TargetSum {
         return helper(nums, remain - nums[i], i + 1) +
                 helper(nums, remain + nums[i], i + 1);
     }
+
+    public int findTargetSumWaysDP(int[] nums, int S) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+
+        if (S > sum || S < -sum) {
+            return 0;
+        }
+
+        int[] dp = new int[2 * sum + 1];
+        dp[sum] = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int[] next = new int[2 * sum + 1];
+
+            for (int k = 0; k < 2 * sum + 1; k++) {
+                if (dp[k] != 0) {
+
+                    next[k + nums[i]] += dp[k];
+                    next[k - nums[i]] += dp[k];
+                }
+            }
+            dp = next;
+        }
+
+        return dp[sum + S];
+    }
+
 }
