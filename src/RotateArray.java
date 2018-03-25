cc3a0d10a75f6de51cb74218f1b045d007c1717f
Herman Wu <zeesun.wu@gmail.com>
2018-03-25 03:27:30 -0700

    rotate array - java

---
+++ src/RotateArray.java
@@ -0,0 +1,36 @@
+/**
+
+ Rotate an array of n elements to the right by k steps.
+
+ For example, with n = 7 and k = 3, the array [1,2,3,4,5,6,7] is rotated to [5,6,7,1,2,3,4].
+
+ Note:
+ Try to come up as many solutions as you can, there are at least 3 different ways to solve this problem.
+
+ [show hint]
+
+ Related problem: Reverse Words in a String II
+
+ Credits:
+ Special thanks to @Freezen for adding this problem and creating all test cases.
+
+ */
+public class RotateArray {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+
+    private void reverse(int[] nums, int left, int right){
+        while (left < right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
