5f05a6d184aabc81a432dda7830aaf9c28db7962
Herman Wu <zeesun.wu@gmail.com>
2018-03-26 01:36:27 -0700

    valid palindrome II

---
+++ src/ValidPalindromeII.java
@@ -0,0 +1,37 @@
+/**
+ Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+ Example 1:
+ Input: "aba"
+ Output: True
+ Example 2:
+ Input: "abca"
+ Output: True
+ Explanation: You could delete the character 'c'.
+
+ */
+public class ValidPalindromeII {
+    public boolean validPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
