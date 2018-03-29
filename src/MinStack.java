e3f13d4493cfafeba19e2ade38eda5575e3c1963
Herman <zeesun.wu@gmail.com>
2018-03-29 13:57:26 -0700

    min stack -java

---
+++ src/MinStack.java
@@ -0,0 +1,22 @@
+/**
+ * Created by hermanwu on 3/29/18.
+ */
+public class MinStack {
+    Stack<Integer> stack=new Stack<>();
+    int min=Integer.MAX_VALUE;
+    public void push(int x) {
+        if(x<=min) {stack.push(min); min=x;}
+        stack.push(x);
+    }
+    public void pop() {
+        if(stack.peek()==min){ stack.pop(); min=stack.pop(); }
+        else stack.pop();
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return min;
+    }
+}
+
