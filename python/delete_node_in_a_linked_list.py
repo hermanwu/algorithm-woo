b91b5bdb64f486833c8c1dac425f71bd5324e90d
Herman Wu <herman.wrt@gmail.com>
2018-07-23 22:33:16 -0700

    implement trie - python

--- python/delete_node_in_a_linked_list.py
+++ python/delete_node_in_a_linked_list.py
@@ -1,4 +1,10 @@
 """
+372. Delete Node in a Linked List
+Implement an algorithm to delete a node in the middle of a singly linked list, given only access to that node.
+
+Example
+Linked list is 1->2->3->4, and given node 3, delete the node in place 1->2->4
+
 Definition of ListNode
 class ListNode(object):
 
@@ -10,7 +16,8 @@ class ListNode(object):
 
 class Solution:
     """
-    @param: node: the node in the list should be deletedt
+    @param: node: the node in the l
+    ist should be deletedt
     @return: nothing
     """
     def deleteNode(self, node):
---
+++ python/implement_trie.py
@@ -0,0 +1,79 @@
+'''
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");
+trie.search("app");     // returns true
+Note:
+
+You may assume that all inputs are consist of lowercase letters a-z.
+All inputs are guaranteed to be non-empty strings.
+'''
+
+class TrieNode:
+    def __init__(self):
+        self.word = False
+        self.children = {}
+
+class Trie:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode()
+
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for i in word:
+            if i not in node.children:
+                node.children[i] = TrieNode()
+            node = node.children[i]
+        node.word = True
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in word:
+            if i not in node.children:
+                return False
+            node = node.children[i]
+        return node.word
+
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for i in prefix:
+            if i not in node.children:
+                return False
+            node = node.children[i]
+        return True
+
+
+    # Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
