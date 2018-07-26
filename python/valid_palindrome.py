d6aacead732555f9d90793fd8f29e4bef48167e2
Herman Wu <herman.wrt@gmail.com>
2018-07-25 17:52:13 -0700

    string - valid palindrome - python

--- python/valid_palindrome.py
+++ python/valid_palindrome.py
@@ -1,11 +1,32 @@
-def isPalindrome(self, s):
-    l, r = 0, len(s)-1
-    while l < r:
-        while l < r and not s[l].isalnum():
-            l += 1
-        while l <r and not s[r].isalnum():
-            r -= 1
-        if s[l].lower() != s[r].lower():
-            return False
-        l +=1; r -= 1
-    return True
+'''
+
+415. Valid Palindrome
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Example
+"A man, a plan, a canal: Panama" is a palindrome.
+
+"race a car" is not a palindrome.
+
+Challenge
+O(n) time without extra memory.
+
+'''
+
+class Solution:
+    def isPalindrome(self, s):
+        l, r = 0, len(s)-1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l <r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l +=1; r -= 1
+        return True
+
+    def isPalindrome2(self, s):
+        # write your code here
+        list = [c for c in s.lower() if c.isalnum()]
+        return list == list[::-1]
