4bee66719b989de6469f3dfb3d77ec4052f31102
Herman Wu <herman.wrt@gmail.com>
2018-09-09 14:05:13 -0700

    wild card matching

--- python/DP/Word/wildcard-matching.py
+++ python/DP/Word/wildcard-matching.py
@@ -19,3 +19,29 @@ isMatch("aab", "c*a*b") → false
 
 '''
 
+class Solution:
+  """
+  @param s: A string
+  @param p: A string includes "?" and "*"
+  @return: is Match?
+  """
+  def isMatch(self, s, p):
+    # write your code here
+    n = len(s)
+    m = len(p)
+    f = [[False for _ in range(m + 1)] for _ in range(n + 1)]
+    f[0][0] = True
+
+    for i in range(0, n + 1):
+      for j in range(0, m + 1):
+        if i > 0 and j > 0:
+          f[i][j] |= f[i - 1][j - 1] and \
+                     (s[i - 1] == p[j - 1] or p[j - 1] in ['?', '*'])
+
+        if i > 0 and j > 0:
+          f[i][j] |= f[i - 1][j] and p[j - 1] == '*'
+
+        if j > 0:
+          f[i][j] |= f[i][j - 1] and p[j - 1] == '*'
+
+    return f[n][m]
--- python/LanguageTricks/create-array.py
+++ python/LanguageTricks/create-array.py
@@ -1,2 +1,14 @@
 def createArray(self, length):
-  return [0 for _ in range(length)]
+  a = [False] * length
+  b = [0 for _ in range(length)]
+
+
+
+'''
+2d array
+https://stackoverflow.com/questions/13382774/initialize-list-with-same-bool-value
+
+'''
+
+def create2DArray(self, m, n):
+  f = [[False for _ in range(m + 1)] for _ in range(n + 1)]
