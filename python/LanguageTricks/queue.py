5d128cac77de324fa4ae7c7a05eb991b33faa130
Herman Wu <herman.wrt@gmail.com>
2018-09-09 22:31:23 -0700

    python queue - moving average from data stream

---
+++ python/LanguageTricks/queue.py
@@ -0,0 +1,8 @@
+def __init__(self):
+  self.queue = list()
+
+def enqueue(self, num):
+  self.queue.append(num)
+
+def dequeue(self, num):
+  self.queue.pop(0)
--- python/Queue/moving-average-from-data-stream.py
+++ python/Queue/moving-average-from-data-stream.py
@@ -1,7 +1,8 @@
 '''
 
 642. Moving Average from Data Stream
-Given a stream of integers and a window size, calculate the moving average of all integers in the sliding window.
+Given a stream of integers and a window size,
+calculate the moving average of all integers in the sliding window.
 
 Example
 MovingAverage m = new MovingAverage(3);
@@ -11,3 +12,24 @@ m.next(3) = (1 + 10 + 3) / 3 // return 4.66667
 m.next(5) = (10 + 3 + 5) / 3 // return 6.00000
 
 '''
+
+class MovingAverage:
+  """
+  @param: size: An integer
+  """
+  def __init__(self, size):
+    self.store = list()
+    self.max_size = size
+    self.sum = 0.0
+
+  """
+  @param: val: An integer
+  @return:
+  """
+  def next(self, val):
+    if len(self.store) == self.max_size:
+      v = self.store.pop(0)
+      self.sum -= v
+    self.sum += val
+    self.store.append(val)
+    return self.sum / len(self.store)
