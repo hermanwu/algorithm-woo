e5ec1827abfaa8b0ce344da210f97c0b73e0b269
Herman Wu <herman.wrt@gmail.com>
2018-09-12 12:44:39 -0700

    isomorphic-string

---
+++ python/Dict/Word/isomorphic-strings.py
@@ -0,0 +1,42 @@
+'''
+
+638. Isomorphic Strings
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+Example
+Given s = "egg", t = "add", return true.
+
+Given s = "foo", t = "bar", return false.
+
+Given s = "paper", t = "title", return true.
+
+Notice
+You may assume both s and t have the same length.
+
+'''
+
+def isIsomorphic(self, s, t):
+  if len(s) != len(t):
+    return False
+
+  dict1 = {}
+  dict2 = {}
+
+  for i in range(len(s)):
+    if s[i] not in dict:
+      dict1[s[i]] = t[i]
+    else:
+      if dict1[s[i]] != t[i]:
+        return False
+
+    if t[i] not in dict2:
+      dict2[t[i]] = s[i]
+    else:
+      if dict2[t[i]] != s[i]:
+        return False
+
+  return True
