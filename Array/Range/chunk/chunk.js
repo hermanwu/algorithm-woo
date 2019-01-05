48b14e254f6b3336bdc8b0fbaa0e1909a009bc85
Herman <zeesun.wu@gmail.com>
2019-01-05 09:35:27 -0800

    javascript chunk

---
+++ Array/Range/chunk/chunk.js
@@ -0,0 +1,27 @@
+function chunk(array, size) {
+  let index = 0;
+  const result = [];
+
+  while (index < array.length) {
+     result.push(array.slice(index, index + size));
+     index = index + size;
+  }
+
+  return result;
+}
+
+function chunk2(array, size) {
+ const chunked = []
+
+ for (let element of array) {
+   const last = chunked[chunked.length - 1];
+
+   if (!last || last.length === size) {
+     chunked.push([element]);
+   } else {
+     last.push(element);
+   }
+ }
+
+ return chunked;
+}
