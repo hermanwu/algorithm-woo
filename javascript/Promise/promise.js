3be7b6f7e978cca477ec9c7d457c6a885f2a76b2
Herman Wu <herman@cloudistics.com>
2019-04-02 18:02:14 -0700

    promises methods

---
+++ javascript/Promise/promise.js
@@ -0,0 +1,22 @@
+// 	Create a function that runs callback after two promises.
+function promises(promise1, promise2, callback) {
+  Promise.all([promise1, promise2]).then((values) => {
+      console.log(values);
+      callback();
+  });
+}
+
+const promise1 = new Promise(function(resolve, reject) {
+  setTimeout(() => { resolve("promise 1") }, 2000);
+})
+
+
+const promise2 = new Promise(function(resolve, reject) {
+  setTimeout(() => { resolve("promise 2") }, 3000);
+})
+
+const callback = () => {
+  console.log('finished');
+}
+
+promises(promise1, promise2, callback);
