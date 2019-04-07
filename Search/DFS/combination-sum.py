da78daa8e6dd1f3833b087841370390c74cde90f
Herman Wu <herman@cloudistics.com>
2019-04-07 13:30:44 -0700

    combination sum

---
+++ Search/DFS/combination-sum.py
@@ -0,0 +1,25 @@
+class Solution:
+  def combinationSum(self, candidates, target):
+    if not candidates:
+      return []
+    
+    candidates.sort()
+    result = []
+    self.dfs(candidates, 0, target, [], result)
+
+
+  def dfs(self, candidates, startIndex, target, localCombos, result):
+    if target == 0:
+      result.append(localCombos[:])
+      return
+    
+    for i in range(startIndex, len(candidates)):
+      if i > startIndex and candidates == candidates[i - 1]:
+        continue
+      
+      if candidates[i] > target:
+        return
+
+    localCombos.append(candidates[i])
+    self.dfs(candidates, i, target - candidates[i], localCombos, result)
+    localCombos.pop()
