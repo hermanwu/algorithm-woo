98af4706fc70d66589efeb0ef004d263af955d4a
Herman Wu <herman@cloudistics.com>
2019-04-14 18:37:45 -0700

    string permutation ii

--- Search/DFS/string-permutation.py
+++ Search/DFS/string-permutation.py
@@ -1,2 +1,23 @@
 class Solution:
-  
+  def stringPermutation2(self, str):
+      if not string:
+        return
+      
+      results = []
+      self.dfs(sorted(list(str)), [], results)
+      return results
+
+  def dfs(self, string, permutation, permutations):
+    if not string:
+      permutations.append(''.join(permutation[:]))
+      return
+
+    for i in range(len(string)):
+      if i and string[i] == string[i - 1]:
+        continue
+      
+      ele = string.pop(i)
+      permutation.append(ele)
+      self.dfs(string, permutation, permutations)
+      permutation.pop()
+      string.inert(i, ele)
