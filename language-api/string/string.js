da16e04bed03b8d2664156de9710420d8a1002f8
Herman Wu <zeesun.wu@gmail.com>
2019-08-28 11:07:09 -0700

    add api to convert number

--- language-api/string/string.js
+++ language-api/string/string.js
@@ -1,7 +1,17 @@
-// find the index of a character a in string b
-let index = b.indexOf(a);
+let expected, input1, input2;
 
+// find the index of a character a in string b
+input1 = 'a';
+input2 = 'bca';
+expected = 2;
+
+console.log(expected === input2.indexOf(input1));
 
 // substring from index 1 to end
-let a = 'test'
+let a = 'test';
 let b = a.substring(1, a.length);
+
+// convert string to number;
+input1 = '12.3';
+expected = 12.3;
+console.log(Number(input1) === expected);
