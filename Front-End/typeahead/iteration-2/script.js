6d24bc5565a3fa7e966a03de7c184c2b8f62c6a0
Herman <zeesun.wu@gmail.com>
2020-07-29 16:49:47 -0700

    add typeahead list creation

--- Front-End/typeahead/iteration-2/script.js
+++ Front-End/typeahead/iteration-2/script.js
@@ -4,3 +4,28 @@ console.log('test 2')
 function myFunction(event) {
   console.log(event)
 }
+
+function showElement(element) {
+  element.style.display = 'block'
+}
+
+function hideElement(element) {
+  element.style.display = 'none'
+}
+
+//hideElement(testDiv);
+
+function makeList(array) {
+  const list = document.createElement('ul');
+  for (let i = 0; i < array.length; i++) {
+    const item = document.createElement('li');
+    item.appendChild(document.createTextNode(array[i]));
+
+    list.appendChild(item);
+  }
+
+  return list;
+}
+
+const list = makeList(['coffee', 'tea']);
+document.getElementsByClassName('option-list')[0].appendChild(list);
