d3f0be31ae945a94b2430afb4d7346dad71715eb
Herman Wu <zeesun.wu@gmail.com>
2020-08-01 14:18:54 -0700

    add debounce function

--- Front-End/typeahead/iteration-2/script.js
+++ Front-End/typeahead/iteration-2/script.js
@@ -1,24 +1,23 @@
-console.log('test 2')
-
+console.log("test 2");
 
 function myFunction(event) {
-  console.log(event)
+  console.log(event);
 }
 
 function showElement(element) {
-  element.style.display = 'block'
+  element.style.display = "block";
 }
 
 function hideElement(element) {
-  element.style.display = 'none'
+  element.style.display = "none";
 }
 
 //hideElement(testDiv);
 
 function makeList(array) {
-  const list = document.createElement('ul');
+  const list = document.createElement("ul");
   for (let i = 0; i < array.length; i++) {
-    const item = document.createElement('li');
+    const item = document.createElement("li");
     item.appendChild(document.createTextNode(array[i]));
 
     list.appendChild(item);
@@ -27,5 +26,23 @@ function makeList(array) {
   return list;
 }
 
-const list = makeList(['coffee', 'tea']);
-document.getElementsByClassName('option-list')[0].appendChild(list);
+const list = makeList(["coffee", "tea"]);
+document.getElementsByClassName("option-list")[0].appendChild(list);
+
+const debounce = (fn, wait) => {
+  let timeout;
+
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => fn.apply(this.args), wait);
+  };
+};
+
+inputBox = document.getElementsByTagName("input")[0];
+
+inputBox.addEventListener(
+  "keydown", // use keydown here. in html is onKeyDown
+  debounce(function () {
+    console.info("Hey! It is", new Date().toUTCString());
+  }, 3000)
+);
