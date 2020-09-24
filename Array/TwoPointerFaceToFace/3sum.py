87fb7630a9451c753e381d1f29ae1b357217f2a5
Herman Wu <zeesun.wu@gmail.com>
2020-09-24 12:22:22 -0700

    3 sum

--- Array/TwoPointerFaceToFace/3sum.py
+++ Array/TwoPointerFaceToFace/3sum.py
@@ -11,11 +11,14 @@ For example, given array S = {-1 0 1 2 -1 -4}, A solution set is:
 (-1, -1, 2)
 
 '''
+
+
 class Solution:
     """
     @param numbers: Give an array numbers of n integer
     @return: Find all unique triplets in the array which gives the sum of zero.
     """
+
     def threeSum(self, nums):
         nums.sort()
         res = []
@@ -39,3 +42,36 @@ class Solution:
                 else:
                     left += 1
         return res
+
+
+class Solution2:
+    """
+    @param numbers: Give an array numbers of n integer
+    @return: Find all unique triplets in the array which gives the sum of zero.
+    """
+
+    def threeSum(self, numbers):
+        # write your code here
+        nums = sorted(numbers)
+
+        results = []
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            self.find_two_sum(nums, i + 1, len(nums) - 1, -nums[i], results)
+
+        return results
+
+    def find_two_sum(self, nums, left, right, target, results):
+        last_pair = None
+        while left < right:
+            if nums[left] + nums[right] == target:
+                if (nums[left], nums[right]) != last_pair:
+                    results.append([-target, nums[left], nums[right]])
+                last_pair = (nums[left], nums[right])
+                right -= 1
+                left += 1
+            elif nums[left] + nums[right] > target:
+                right -= 1
+            else:
+                left += 1
