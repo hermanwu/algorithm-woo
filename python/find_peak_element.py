5ea15f384be4d80bb0261165807269daf7309ff1
Herman Wu <zeesun.wu@gmail.com>
2020-10-17 13:39:15 -0700

    find peak element

--- python/find_peak_element.py
+++ python/find_peak_element.py
@@ -18,15 +18,17 @@ Challenge
 Time complexity O(logN)
 '''
 
+
 class Solution:
     """
     @param: A: An integers array.
     @return: return any of peek positions.
     """
+
     def findPeak(self, A):
         # write your code here
         start, end = 1, len(A) - 2
-        while start + 1 <  end:
+        while start + 1 < end:
             mid = (start + end) // 2
             if A[mid - 1] <= A[mid] and A[mid] <= A[mid + 1]:
                 start = mid
