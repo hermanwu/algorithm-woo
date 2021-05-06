88e6d15ebc3910afe4eb874b1d3febbe009e503a
Herman Wu <herman@cloudistics.com>
2021-05-06 15:00:00 -0700

    add tree recursion methodology

--- Tree/BST/Delete-node/delete-node-in-a-bst.ts
+++ Tree/BST/Delete-node/delete-node-in-a-bst.ts
@@ -21,20 +21,37 @@ export function predecessor(root: TreeNode) {
   return root.val;
 }
 
+/**
+ * Methodology: Tree Recursion.
+ * 1. Design a recursive call that pass in enough information (Whether those information is good to store globally).
+ * 2. For simple purpose, you don't need to use method itself.
+ * 3. In the recursive method, handle null condition and terminate condition.
+ * 4. Divide and conquer:  handle three branches: Left child node, right child node, and middle node (order can definitely be changed).
+ * 5. Each branch should have terminate condition and move to next level.
+ * @param root
+ * @param key
+ * @returns
+ */
 function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
-  // Handle null.
+  //  Design a recursive call that pass in enough information (Whether those information is good to store globally).
+  return helper(root, key);
+}
+
+function helper(root: TreeNode | null, key: number): TreeNode | null {
+  // In the recursive method, handle null condition and terminate condition.
   if (root === null) {
     return null;
   }
 
+  // Divide and conquer:  handle three branches: Left child node, right child node, and middle node (order can definitely be changed).
   // Handle left node
   if (key > root.val) {
-    root.right = deleteNode(root.right, key);
+    root.right = helper(root.right, key);
   }
 
   // Handle right node.
   else if (key < root.val) {
-    root.left = deleteNode(root.left, key);
+    root.left = helper(root.left, key);
   } else {
     // Handle both null.
     // Terminate condition.
@@ -42,10 +59,10 @@ function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
       root = null;
     } else if (root.right != null) {
       root.val = successor(root);
-      root.right = deleteNode(root.right, root.val);
+      root.right = helper(root.right, root.val);
     } else {
       root.val = predecessor(root);
-      root.right = deleteNode(root.left, root.val);
+      root.right = helper(root.left, root.val);
     }
   }
 
