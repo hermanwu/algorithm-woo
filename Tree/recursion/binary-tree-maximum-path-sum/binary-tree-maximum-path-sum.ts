0a7e5008df2aa0e4699965d64a737564a0656b49
Herman Wu <herman@cloudistics.com>
2021-05-07 14:00:10 -0700

    add max path sum question

---
+++ Tree/recursion/binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.ts
@@ -0,0 +1,35 @@
+/**
+Binary Tree Maximum Path Sum.
+
+https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
+A path in a binary tree is a sequence of nodes where each pair of adjacent nodes in the sequence has an edge connecting them. A node can only appear in the sequence at most once. Note that the path does not need to pass through the root.
+
+The path sum of a path is the sum of the node's values in the path.
+
+Given the root of a binary tree, return the maximum path sum of any path.
+
+
+Input: root = [1,2,3]
+Output: 6
+Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+
+
+Input: root = [-10,9,20,null,null,15,7]
+Output: 42
+Explanation: The optimal path is 15 -> 20 -> 7 with a path sum of 15 + 20 + 7 = 42.
+
+ */
+
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+  constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+  }
+}
+
+function maxPathSum(root: TreeNode | null): number {}
