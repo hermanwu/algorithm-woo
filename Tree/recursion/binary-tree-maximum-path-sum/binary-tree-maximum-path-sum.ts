f6a43a357323111b385b08500dfb302d54ef1b01
Herman Wu <zeesun.wu@gmail.com>
2021-05-11 15:01:52 -0700

    add coin change

--- Tree/recursion/binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.ts
+++ Tree/recursion/binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.ts
@@ -32,4 +32,41 @@ class TreeNode {
   }
 }
 
-function maxPathSum(root: TreeNode | null): number {}
+/**
+ * Methodology: Tree Recursion.
+ * 1. Figure out what information needs to be stored globally.
+ * 1. Design a recursive call that get enough information and
+ *    return the result that can be used recursively
+ *
+ * 2. For simple purpose, an helper function could always be useful.
+ * 3. In the recursive method, handle null condition and terminate condition.
+ * 4. Divide:  handle three branches: Left child node, right child node, and middle node (order can definitely be changed).
+ * 5  Gather necessary information with branch results.
+ * 5. Conquer.
+ */
+function maxPathSum(root: TreeNode | null): number {
+  // Figure out what information needs to be stored globally
+  let max = Number.NEGATIVE_INFINITY;
+
+  // Design a recursive call that get enough information and return the result that can be used recursively
+  function helper(node: TreeNode): number {
+    // In the recursive method, handle null condition and terminate condition.
+    if (node === null) {
+      return 0;
+    }
+
+    // Divide:  handle three branches: Left child node, right child node, and middle node (order can definitely be changed).
+    const left = Math.max(0, helper(node.left));
+    const right = Math.max(0, helper(node.right));
+
+    // Gather necessary information with branch results.
+    max = Math.max(max, left + right + node.val);
+
+    // Conquer
+    return Math.max(left, right) + node.val;
+  }
+
+  helper(root);
+
+  return max;
+}
