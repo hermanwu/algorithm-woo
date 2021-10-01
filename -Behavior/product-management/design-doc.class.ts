7b0049736ab70a7fe5060094355b135ddc787a34
Herman Wu <zeesun.wu@gmail.com>
2021-09-30 17:43:59 -0700

    add system design classes

---
+++ -Behavior/product-management/design-doc.class.ts
@@ -0,0 +1,14 @@
+export class designDoc {
+  projectValue;
+  backgroundAndIntroduction;
+  Requirements;
+  nonFunctionalRequirement;
+  proposedSolution;
+  alternativeSolution;
+  testingPlan;
+  testingMetric;
+  monitoringPlan;
+  workItemTime;
+  meetingNotes;
+  references;
+}
---
+++ -Behavior/project-management/process-design.class.ts
@@ -0,0 +1,5 @@
+export class Process {
+  communicate() {
+    return "details, the more the better, planning, streamline with different team";
+  }
+}
