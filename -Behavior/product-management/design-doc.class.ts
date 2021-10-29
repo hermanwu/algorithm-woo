cc283e840a951d65e31ca3f260394b4ef1f58cde
Herman Wu <zeesun.wu@gmail.com>
2021-10-29 16:06:36 -0700

    upgrade design doc

--- -Behavior/product-management/design-doc.class.ts
+++ -Behavior/product-management/design-doc.class.ts
@@ -1,18 +1,17 @@
 export class designDoc {
-  priority;
-  backgroundAndIntroduction;
-  projectValue;
-  Requirements;
-  nonFunctionalRequirement;
-  excludingScope;
-  proposedSolution;
-  alternativeSolution;
-  taskOrder;
-  estimateTime;
-  testingPlan;
-  testingMetric;
-  monitoringPlan;
-  workItemTime;
-  meetingNotes;
-  references;
+  MotivationAndPurpose;
+  RequirementsAndPriority;
+  ExcludingScope;
+
+  ProposedImplementation;
+  AlternativeImplementation;
+  TaskOrderAndEstimateTime;
+
+  TestingPlanAndMetric;
+  MonitoringPlan;
+  ValueCreated;
+
+  NotesAndQuestions;
+  References;
+  Dcouments;
 }
